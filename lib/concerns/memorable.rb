2974fcedcc8112336bf7166b179e47bf47c5577f
Josh Burd <Jburdt@gmail.com>
2022-11-29 12:39:07 -0500

    commit tree b94f17d789650f4fd7ed23ca05ab4fa4d3b1c87c

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/concerns/memorable'
+
+module Memorable
+ module ClassMethods
+  
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+end
+
+end
